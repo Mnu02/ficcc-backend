0a9bad54052280e1a48f62c7e826a54c35429e31
Franklin Moyo <115519540+Mnu02@users.noreply.github.com>
2026-04-23 13:21:03 -0400

    Add description field to Event model

--- models/event.go
+++ models/event.go
@@ -4,10 +4,11 @@ import "time"
 
 // Event represents a church event
 type Event struct {
-	ID       string     `json:"id"`
-	Name     string     `json:"name"`
-	Location string     `json:"location"`
-	ImageURL *string    `json:"image_url"`
-	StartsAt time.Time  `json:"starts_at"`
-	EndsAt   *time.Time `json:"ends_at"`
+	ID          string     `json:"id"`
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+	Location    string     `json:"location"`
+	ImageURL    *string    `json:"image_url"`
+	StartsAt    time.Time  `json:"starts_at"`
+	EndsAt      *time.Time `json:"ends_at"`
 }
